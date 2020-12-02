c697676125784b506594c3dc73630e2d6425d070
Marc Armgren <marc.armgren@volumental.com>
2020-12-02 17:05:42 +0100

    asd

---
+++ sources/OBJCExampleClass.h
@@ -0,0 +1,14 @@
+//
+//  OBJCExampleClass.h
+//  Example-Swift-Package-Manager
+//
+//  Created by marc armgren on 2020-12-02.
+//
+
+#ifndef OBJCExampleClass_h
+#define OBJCExampleClass_h
+
+#endif /* OBJCExampleClass_h */
+
+@interface OBJCExampleClass : NSObject
+@end
---
+++ sources/OBJCExampleClass.m
@@ -0,0 +1,15 @@
+//
+//  OBJCExampleClass.m
+//  Example-Swift-Package-Manager
+//
+//  Created by marc armgren on 2020-12-02.
+//
+
+#import <Foundation/Foundation.h>
+#import "OBJCExampleClass.h"
+
+@implementation OBJCExampleClass
++ (NSString *)asd {
+    return @"test";
+}
+@end
