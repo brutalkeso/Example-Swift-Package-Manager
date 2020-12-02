51f40202dfe02eb79d1a2cfc3fc9cc6ca6a41fc5
Marc Armgren <marc.armgren@volumental.com>
2020-12-02 17:33:31 +0100

    asd

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Example-Swift-Package-Manager",
     products: [
-        .library(name: "Example-Swift-Package-Manager", .type: .dynamic, targets: ["Example-Swift-Package-Manager"])
+        .library(name: "Example-Swift-Package-Manager", type: .dynamic, targets: ["Example-Swift-Package-Manager"]),
     ],
     targets: [
         .target(name: "Example-Swift-Package-Manager", dependencies: [], path: "sources"),
